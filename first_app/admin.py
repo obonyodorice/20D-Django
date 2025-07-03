1524e2729bdadc83c17e75c8b4ebbda4b00d2c1a
obonyodorice <obonyodorice3@gmail.com>
2025-07-02 21:37:09 -0700

    cm

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-# from .models import Attachee
+from .models import Attachee
 # Register your models here.
-# admin.site.register(Attachee)
+admin.site.register(Attachee)
--- first_app/views.py
+++ first_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-# from .models import Attachee
+from .models import Attachee
 
-# def index(request):
-#     return render(request, 'first_app/index.html')
+def index(request):
+    return render(request, 'first_app/index.html')
 
-# def attachee_view(request):
-#     attachee_data = Attachee.objects.all()
+def attachee_view(request):
+    attachee_data = Attachee.objects.all()
 
-#     context = {
-#         'attachee_data': attachee_data
-#     }
+    context = {
+        'attachee_data': attachee_data
+    }
 
-#     return render(request, 'first_app/attachee_view.html', context)
+    return render(request, 'first_app/attachee_view.html', context)
