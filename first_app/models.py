34c174ee8e91ab8e6f814f1274ee7c14e89c396a
obonyodorice <obonyodorice3@gmail.com>
2025-07-02 21:35:54 -0700

    cm models

--- first_app/models.py
+++ first_app/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
+from django.db import models
 
-# class Attachee(models.Model):
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField()
-#     phone = models.CharField(max_length=15)
-#     university = models.CharField(max_length=100)
-#     department = models.CharField(max_length=100)
+class Attachee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    university = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
