d127657aca074c1f3b154fcf1100d20a46bbb5fd
obonyodorice <obonyodorice3@gmail.com>
2025-07-07 11:48:25 -0700

    cm

--- second_app/models.py
+++ second_app/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# class BorrowedBook(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     isbn = models.CharField(max_length=13, unique=True, help_text="International Standard Book Number")
+#     borrower_name = models.CharField(max_length=100)
+#     borrow_date = models.DateField(auto_now_add=True)
+#     return_date = models.DateField(null=True, blank=True)
+#     is_returned = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return f"{self.title} by {self.author} (Borrowed by {self.borrower_name})"
+
+#     class Meta:
+#         verbose_name = "Borrowed Library Book"
+#         verbose_name_plural = "Borrowed Library Books"
+#         ordering = ['-borrow_date'] 
--- second_app/urls.py
+++ second_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-]
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('borrowed_books/', views.borrowed_books, name='borrowed_books'),   
+# ]
