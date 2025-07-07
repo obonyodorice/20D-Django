50428fb7362e2dbeab905104b75760d01cf09413
obonyodorice <obonyodorice3@gmail.com>
2025-07-07 11:59:30 -0700

    cm

--- second_app/views.py
+++ second_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import BorrowedBook
 
 def home(request):
     return render(request, 'second_app/home.html')
 
+def borrowed_books(request):
+    borrowed_books_data = BorrowedBook.objects.all()
+
+    context = {
+        'borrowed_books_data': borrowed_books_data
+    }
+
+    return render(request, 'second_app/borrowed_books.html', context)
